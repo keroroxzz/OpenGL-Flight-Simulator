d16c3d17af038c31110cdc2bbdd8f9cf042514cb
Brian Tu <fumincha763@gmail.com>
2024-04-19 23:15:36 +0800

    Add path manager

---
+++ utils/path_mgr/includes/path_mgr.h
@@ -0,0 +1,24 @@
+#include <libgen.h>
+#include <linux/limits.h>
+#include <unistd.h>
+#include <iostream>
+
+#ifndef PATH_MGR_H
+#define PATH_MGR_H
+
+// #ifndef EXEC_PATH
+// extern const std::string exec_path;
+// #endif // EXEC_PATH
+
+struct Path{
+private:
+    static std::string exec_path;
+    std::string path;
+
+public:
+
+    Path(const char *path);
+    operator const char *();
+};
+
+#endif // PATH_MGR_H
---
+++ utils/path_mgr/src/path_mgr.cpp
@@ -0,0 +1,33 @@
+// #define EXEC_PATH
+#include "utils/path_mgr/includes/path_mgr.h"
+#include "utils/logger/includes/logger.h"
+
+USE_LOGS();
+
+// const std::string exec_path;
+
+std::string Path::exec_path = "";
+
+Path::Path(const char *path): path(path){
+
+  // initialize the path of the executable file
+  if (exec_path.size() == 0) {
+    char result[PATH_MAX];
+    ssize_t count = readlink("/proc/self/exe", result, PATH_MAX);
+    if (count != -1) {
+      exec_path = std::string(dirname(result));
+      LOGD("exec path: %s", exec_path.c_str());
+    } else {
+      LOGE("readlink failed");
+    }
+  }
+
+  if (this->path[0] == '.'){
+    this->path = exec_path + this->path.substr(1);
+  }
+  LOGD("path: %s", path);
+}
+
+Path::operator const char *(){
+  return this->path.c_str();
+}
